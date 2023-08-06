3f8ccd8a660956fcc3b8de21e3e92474b07ed698
fabian <mo@merasotech.de>
2023-08-06 23:43:20 +0200

    implement login && delete secrets and Ids for security

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -8,6 +8,7 @@ import AuthSocialButton from "./AuthSocialButton";
 import {BsGithub, BsGoogle} from "react-icons/bs";
 import axios from "axios";
 import toast from "react-hot-toast";
+import {signIn} from 'next-auth/react'
 
 type Variant = 'LOGIN' | 'REGISTER'
 
@@ -49,14 +50,36 @@ const AuthForm = () => {
         }
 
         if (variant === 'LOGIN') {
-            // Next Auth SignIn
+            signIn('credentials', {
+                ...data,
+                redirect: false
+            })
+                .then((callback) => {
+                    if (callback?.error){
+                        toast.error('Invalid credentials')
+                    }
+                    if (callback?.ok && !callback?.error){
+                        toast.success('Logged in!')
+                    }
+                })
+                .finally(() => setIsLoading(false))
         }
     }
 
     const socialAction = (action: string) => {
         setIsLoading(true);
-
-        // Next Auth Social Sign In
+        signIn(action, {
+            redirect: false
+        })
+            .then((callback) => {
+                if (callback?.error){
+                    toast.error('Invalid credentials')
+                }
+                if (callback?.ok && !callback?.error){
+                    toast.success('Logged in!')
+                }
+            })
+            .finally(() => setIsLoading(false))
     }
 
     return (
